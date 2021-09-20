7ef5b7fe3771cafc9d529e8303a4564454fc6027
HPJames <90324929+HPJames@users.noreply.github.com>
2021-09-20 10:33:59 +0400

    Update enable-push.js

--- enable-push.js
+++ enable-push.js
@@ -1,304 +1,298 @@
 "use strict";
 
 let swReady = null,
-    baseURL = "",
-    hasLoaded = false,
-    pushType = "",
-    vPublicKey = "",
-    aPushId = "",
-    aDeviceToken = "";
+	baseURL = "",
+	hasLoaded = false,
+	pushType = "",
+	vPublicKey = "",
+	aPushId = "",
+	aDeviceToken = "";
 
 document.addEventListener("DOMContentLoaded", initSW);
 window.onload = initSW; //fallback.
 
 async function initDB(data) {
-    let _pushDB_ = null;
-    let _pushDBName_ = "_PUSH_DB_";
-    let _storeName_ = "_PUSH_STORE_";
+	let _pushDB_ = null;
+	let _pushDBName_ = "_PUSH_DB_";
+	let _storeName_ = "_PUSH_STORE_";
 
-    if (!window.indexedDB) {
-        console.log("IndexDB not supported.");
-        return;
-    }
+	if (!window.indexedDB) {
+		console.log("IndexDB not supported.");
+		return;
+	}
 
-    var request = window.indexedDB.open(_pushDBName_, 1);
+	var request = window.indexedDB.open(_pushDBName_, 1);
 
-    request.onerror = function(event) {
-        console.log("Unable to open new db.");
-    };
+	request.onerror = function (event) {
+		console.log("Unable to open new db.");
+	};
 
-    request.onupgradeneeded = async function(event) {
-        _pushDB_ = event.target.result;
+	request.onupgradeneeded = async function (event) {
+		_pushDB_ = event.target.result;
 
-        var objectStore = _pushDB_.createObjectStore(_storeName_, {
-            keyPath: "id"
-        });
+		var objectStore = _pushDB_.createObjectStore(_storeName_, {
+			keyPath: "id",
+		});
 
-        objectStore.createIndex("value", "value", {
-            unique: false
-        });
+		objectStore.createIndex("value", "value", {
+			unique: false,
+		});
 
-        console.log("DB created/upgraded:");
+		objectStore.transaction.oncomplete = function (event) {
+			// Store values in the newly created objectStore.
+			var storeOBJ = _pushDB_
+				.transaction(_storeName_, "readwrite")
+				.objectStore(_storeName_);
+			storeOBJ.add(data);
+		};
+	};
 
-        objectStore.transaction.oncomplete = function(event) {
-            console.log("Store created/opened:");
-            // Store values in the newly created objectStore.
-            var storeOBJ = _pushDB_
-                .transaction(_storeName_, "readwrite")
-                .objectStore(_storeName_);
-            storeOBJ.add(data);
-        };
-    };
-
-    request.onsuccess = function(event) {
-        // _pushDB_ = event.target.result;
-        console.log("DB initialized.");
-    };
+	request.onsuccess = function (event) {
+		// _pushDB_ = event.target.result;
+	};
 }
 
 // Register service worker
 async function initSW() {
-    // Prevent from initializing multiple times
-    if (hasLoaded) return;
-    hasLoaded = true;
+	// Prevent from initializing multiple times
+	if (hasLoaded) return;
+	hasLoaded = true;
 
-    // Initialize base url
-    if (typeof _PUSH_BASE_URL_ !== "undefined") {
-        baseURL = _PUSH_BASE_URL_;
-    }
+	// Initialize base url
+	if (typeof _PUSH_BASE_URL_ !== "undefined") {
+		baseURL = _PUSH_BASE_URL_;
+	}
 
-    if (
-        !"serviceWorker" in navigator &&
-        !"PushManager" in window &&
-        !"safari" in window &&
-        !"pushNotification" in window.safari
-    ) {
-        console.log("[Web Push] not supported.");
-        return;
-    } else if ("serviceWorker" in navigator && "PushManager" in window) {
-        //Set to web push api
-        pushType = "api";
+	if (
+		!"serviceWorker" in navigator &&
+		!"PushManager" in window &&
+		!"safari" in window &&
+		!"pushNotification" in window.safari
+	) {
+		console.log("[Web Push] not supported.");
+		return;
+	} else if ("serviceWorker" in navigator && "PushManager" in window) {
+		//Set to web push api
+		pushType = "api";
 
-        // register the service worker
-        await navigator.serviceWorker
-            .register(window.location.origin + "/pf-sw.js")
-            .then(() => {
-                console.log("[Service Worker] Registered.");
-                initWebPush();
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    } else if ("safari" in window && "pushNotification" in window.safari) {
-        //Set to apple apn gateway
-        pushType = "apn";
+		// register the service worker
+		await navigator.serviceWorker
+			.register(window.location.origin + "/pf-sw.js")
+			.then(() => {
+				initWebPush();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	} else if ("safari" in window && "pushNotification" in window.safari) {
+		//Set to apple apn gateway
+		pushType = "apn";
 
-        initApplePush();
-    }
+		initApplePush();
+	}
 }
 
 async function initWebPush() {
-    swReady = await navigator.serviceWorker.ready;
+	swReady = await navigator.serviceWorker.ready;
 
-    if (!swReady) {
-        console.log("[Service Worker] not ready.");
-        return;
-    }
+	if (!swReady) {
+		console.log("[Service Worker] not ready.");
+		return;
+	}
 
-    // Skip if subscription already exists.
-    let subscriptionExists = await swReady.pushManager.getSubscription();
-    if (subscriptionExists) return;
+	// Skip if subscription already exists.
+	let subscriptionExists = await swReady.pushManager.getSubscription();
+	if (subscriptionExists) return;
 
-    let optinDialog = await getOptinDialog();
-    vPublicKey = optinDialog.vPublicKey;
+	let optinDialog = await getOptinDialog();
+	vPublicKey = optinDialog.vPublicKey;
 
-    if (optinDialog.success) {
-        showOptinDialog(optinDialog);
-        return;
-    }
-    // Proceed on getting notification permission.
-    getNotificationPermission();
+	if (optinDialog.success) {
+		showOptinDialog(optinDialog);
+		return;
+	}
+	// Proceed on getting notification permission.
+	getNotificationPermission();
 }
 
 async function initApplePush() {
-    let optinDialog = await getOptinDialog();
-    aPushId = optinDialog.aPushId;
+	let optinDialog = await getOptinDialog();
+	aPushId = optinDialog.aPushId;
 
-    if (optinDialog.success) {
-        showOptinDialog(optinDialog);
-        return;
-    }
-    // Proceed on getting notification permission.
-    getNotificationPermission();
+	if (optinDialog.success) {
+		showOptinDialog(optinDialog);
+		return;
+	}
+	// Proceed on getting notification permission.
+	getNotificationPermission();
 }
 
 async function getOptinDialog() {
-    var requestURL = baseURL + "/api/dialog/double-optin-script";
-    var chGUID = "";
+	var requestURL = baseURL + "/api/dialog/double-optin-script";
+	var chGUID = "";
 
-    if (typeof _PUSH_CHANNEL_GUID_ !== "undefined") {
-        chGUID = _PUSH_CHANNEL_GUID_;
-    }
+	if (typeof _PUSH_CHANNEL_GUID_ !== "undefined") {
+		chGUID = _PUSH_CHANNEL_GUID_;
+	}
 
-    let response = null;
+	let response = null;
 
-    await fetch(requestURL, {
-        method: "POST",
-        body: JSON.stringify({ channelGUID: chGUID }),
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        }
-    })
-        .then(resp => {
-            response = resp.json();
-        })
-        .catch(err => {
-            console.log("Get Optin Dialog: ", err);
-        });
+	await fetch(requestURL, {
+		method: "POST",
+		body: JSON.stringify({ channelGUID: chGUID }),
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+	})
+		.then((resp) => {
+			response = resp.json();
+		})
+		.catch((err) => {
+			console.log("Get Optin Dialog: ", err);
+		});
 
-    return response;
+	return response;
 }
 
 async function showOptinDialog(optinDialog) {
-    if (optinDialog.popup) {
-        // Set correct dialog details
-        optinDialog.popup = optinDialog.popup.replace(
-            "{{icon}}",
-            optinDialog.dialog.icon
-        );
-        optinDialog.popup = optinDialog.popup.replace(
-            "{{title}}",
-            optinDialog.dialog.title
-        );
-        optinDialog.popup = optinDialog.popup.replace(
-            "{{body}}",
-            optinDialog.dialog.body
-        );
-        optinDialog.popup = optinDialog.popup.replace(
-            "{{ok_text}}",
-            optinDialog.dialog.acceptText
-        );
-        optinDialog.popup = optinDialog.popup.replace(
-            "{{cancel_text}}",
-            optinDialog.dialog.denyText
-        );
+	if (optinDialog.popup) {
+		// Set correct dialog details
+		optinDialog.popup = optinDialog.popup.replace(
+			"{{icon}}",
+			optinDialog.dialog.icon
+		);
+		optinDialog.popup = optinDialog.popup.replace(
+			"{{title}}",
+			optinDialog.dialog.title
+		);
+		optinDialog.popup = optinDialog.popup.replace(
+			"{{body}}",
+			optinDialog.dialog.body
+		);
+		optinDialog.popup = optinDialog.popup.replace(
+			"{{ok_text}}",
+			optinDialog.dialog.acceptText
+		);
+		optinDialog.popup = optinDialog.popup.replace(
+			"{{cancel_text}}",
+			optinDialog.dialog.denyText
+		);
 
-        document.body.innerHTML += optinDialog.popup;
-        var dialog = document.getElementById("md-slidedown-container");
-        dialog.style.display = "block";
-        var acceptBtn = document.getElementById("md-slidedown-allow-button");
-        var denyBtn = document.getElementById("md-slidedown-cancel-button");
-        let dialogResult = { dialogID: optinDialog.dialog.id };
+		document.body.innerHTML += optinDialog.popup;
+		var dialog = document.getElementById("md-slidedown-container");
+		dialog.style.display = "block";
+		var acceptBtn = document.getElementById("md-slidedown-allow-button");
+		var denyBtn = document.getElementById("md-slidedown-cancel-button");
+		let dialogResult = { dialogID: optinDialog.dialog.id };
 
-        acceptBtn.onclick = async () => {
-            dialogResult.result = "accepted";
-            getOptinDialogResult(dialog, dialogResult);
-        };
-        denyBtn.onclick = async () => {
-            dialogResult.result = "denied";
-            getOptinDialogResult(dialog, dialogResult);
-        };
-    }
+		acceptBtn.onclick = async () => {
+			dialogResult.result = "accepted";
+			getOptinDialogResult(dialog, dialogResult);
+		};
+		denyBtn.onclick = async () => {
+			dialogResult.result = "denied";
+			getOptinDialogResult(dialog, dialogResult);
+		};
+	}
 }
 
 async function getOptinDialogResult(dialog = null, optinResult = {}) {
-    if (dialog !== null) {
-        dialog.style.display = "none";
-    }
+	if (dialog !== null) {
+		dialog.style.display = "none";
+	}
 
-    if (typeof optinResult.dialogID !== "undefined") {
-        fetch(baseURL + "/api/dialog/result", {
-            method: "POST",
-            body: JSON.stringify(optinResult),
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        })
-            .then(resp => {
-                return resp.json();
-            })
-            .then(response => {
-                if (!response.success) {
-                    throw new Error("Unable to record dialog result.");
-                }
-            })
-            .catch(err => {
-                console.log("Get Optin Dialog Result: ", err);
-            });
+	if (typeof optinResult.dialogID !== "undefined") {
+		fetch(baseURL + "/api/dialog/result", {
+			method: "POST",
+			body: JSON.stringify(optinResult),
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((response) => {
+				if (!response.success) {
+					throw new Error("Unable to record dialog result.");
+				}
+			})
+			.catch((err) => {
+				console.log("Get Optin Dialog Result: ", err);
+			});
 
-        // Ask for notification permission only if accepted.
-        if (optinResult.result === "accepted") {
-            await getNotificationPermission();
-        }
-    }
+		// Ask for notification permission only if accepted.
+		if (optinResult.result === "accepted") {
+			await getNotificationPermission();
+		}
+	}
 }
 
 function getNotificationPermission() {
-    if (pushType === "api") {
-        new Promise(function(resolve, reject) {
-            const permissionResult = Notification.requestPermission(function(
-                result
-            ) {
-                resolve(result);
-            });
+	if (pushType === "api") {
+		new Promise(function (resolve, reject) {
+			const permissionResult = Notification.requestPermission(function (
+				result
+			) {
+				resolve(result);
+			});
 
-            if (permissionResult) {
-                permissionResult.then(resolve, reject);
-            }
-        }).then(permissionResult => {
-            if (permissionResult !== "granted") {
-                throw new Error("We weren't granted permission.");
-            }
-            console.log("Permission accepted.");
-            subscribeUser();
-        });
-    } else if (pushType === "apn") {
-        console.log("Safari push permission.");
-        var permissionData = window.safari.pushNotification.permission(aPushId);
-        checkRemotePermission(permissionData);
-    }
+			if (permissionResult) {
+				permissionResult.then(resolve, reject);
+			}
+		}).then((permissionResult) => {
+			if (permissionResult !== "granted") {
+				throw new Error("We weren't granted permission.");
+			}
+			console.log("Permission accepted.");
+			subscribeUser();
+		});
+	} else if (pushType === "apn") {
+		console.log("Safari push permission.");
+		var permissionData = window.safari.pushNotification.permission(aPushId);
+		checkRemotePermission(permissionData);
+	}
 }
 
-var checkRemotePermission = function(permissionData) {
-    if (permissionData.permission === "default") {
-        // This is a new web service URL and its validity is unknown.
-        window.safari.pushNotification.requestPermission(
-            baseURL, // The web service URL.
-            aPushId, // The Website Push ID.
-            {}, // Data that you choose to send to your server to help you identify the user.
-            checkRemotePermission // The callback function.
-        );
-    } else if (permissionData.permission === "denied") {
-        throw new Error("We weren't granted permission.");
-    } else if (permissionData.permission === "granted") {
-        aDeviceToken = permissionData.deviceToken;
-        console.log("Permission accepted.", aDeviceToken);
-        // The web service URL is a valid push provider, and the user said yes.
-        // permissionData.deviceToken is now available to use.
-    }
-    console.log(permissionData);
+var checkRemotePermission = function (permissionData) {
+	if (permissionData.permission === "default") {
+		// This is a new web service URL and its validity is unknown.
+		window.safari.pushNotification.requestPermission(
+			baseURL, // The web service URL.
+			aPushId, // The Website Push ID.
+			{}, // Data that you choose to send to your server to help you identify the user.
+			checkRemotePermission // The callback function.
+		);
+	} else if (permissionData.permission === "denied") {
+		throw new Error("We weren't granted permission.");
+	} else if (permissionData.permission === "granted") {
+		aDeviceToken = permissionData.deviceToken;
+		console.log("Permission accepted.", aDeviceToken);
+		// The web service URL is a valid push provider, and the user said yes.
+		// permissionData.deviceToken is now available to use.
+	}
+	console.log(permissionData);
 };
 
 /**
  * Subscribe user to push
  */
 async function subscribeUser() {
-    const subscribeOptions = {
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vPublicKey)
-    };
+	const subscribeOptions = {
+		userVisibleOnly: true,
+		applicationServerKey: urlBase64ToUint8Array(vPublicKey),
+	};
 
-    let newSubscription = await swReady.pushManager.subscribe(subscribeOptions);
+	let newSubscription = await swReady.pushManager.subscribe(subscribeOptions);
 
-    await swReady.active.postMessage({
-        endpoint: newSubscription.endpoint,
-        subscribeOptions: subscribeOptions,
-        baseURL: baseURL
-    });
-    console.log("Posted message");
-    storePushSubscription(newSubscription);
+	await swReady.active.postMessage({
+		endpoint: newSubscription.endpoint,
+		subscribeOptions: subscribeOptions,
+		baseURL: baseURL,
+	});
+	storePushSubscription(newSubscription);
 }
 
 /**
@@ -306,42 +300,40 @@ async function subscribeUser() {
  * @param {object} pushSubscription
  */
 function storePushSubscription(pushSubscription = {}) {
-    var custom_tag = "";
+	var custom_tag = "";
 
-    if (typeof _PUSH_CUSTOM_TAG_ !== "undefined") {
-        custom_tag = _PUSH_CUSTOM_TAG_;
-    }
+	if (typeof _PUSH_CUSTOM_TAG_ !== "undefined") {
+		custom_tag = _PUSH_CUSTOM_TAG_;
+	}
 
-    var pushSubsSTR = JSON.stringify(pushSubscription);
-    var pushSUBsOBJ = JSON.parse(pushSubsSTR);
-    pushSUBsOBJ.pushCustomTag = custom_tag;
-    pushSUBsOBJ.timezone = new Date().toTimeString().split(" ")[1];
-    pushSUBsOBJ.domain = window.location.origin;
-    if (typeof pushSUBsOBJ.endpoint === "undefined") {
-        pushSUBsOBJ.endpoint = aDeviceToken;
-    }
+	var pushSubsSTR = JSON.stringify(pushSubscription);
+	var pushSUBsOBJ = JSON.parse(pushSubsSTR);
+	pushSUBsOBJ.pushCustomTag = custom_tag;
+	pushSUBsOBJ.timezone = new Date().toTimeString().split(" ")[1];
+	pushSUBsOBJ.domain = window.location.origin;
+	if (typeof pushSUBsOBJ.endpoint === "undefined") {
+		pushSUBsOBJ.endpoint = aDeviceToken;
+	}
 
-    fetch(baseURL + "/api/subscribepush", {
-        method: "POST",
-        body: JSON.stringify(pushSUBsOBJ),
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        }
-    })
-        .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            if (res.success && res.data) {
-                console.log("Subscribed!");
-                console.log(res.data);
-                initDB({ id: "endpoint", value: res.data.endpoint });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+	fetch(baseURL + "/api/subscribepush", {
+		method: "POST",
+		body: JSON.stringify(pushSUBsOBJ),
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			if (res.success && res.data) {
+				initDB({ id: "endpoint", value: res.data.endpoint });
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
 /**
@@ -350,16 +342,16 @@ function storePushSubscription(pushSubscription = {}) {
  * @param {string} base64String a public vapid key
  */
 function urlBase64ToUint8Array(base64String) {
-    var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-    var base64 = (base64String + padding)
-        .replace(/\-/g, "+")
-        .replace(/_/g, "/");
+	var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+	var base64 = (base64String + padding)
+		.replace(/\-/g, "+")
+		.replace(/_/g, "/");
 
-    var rawData = window.atob(base64);
-    var outputArray = new Uint8Array(rawData.length);
+	var rawData = window.atob(base64);
+	var outputArray = new Uint8Array(rawData.length);
 
-    for (var i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
+	for (var i = 0; i < rawData.length; ++i) {
+		outputArray[i] = rawData.charCodeAt(i);
+	}
+	return outputArray;
 }
